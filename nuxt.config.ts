0a9ebea560deaf88429be381adba469e5f07eed9
Tolu ISOLA <i_tolu@yahoo.com>
2024-04-08 12:53:27 +0100

    :bug: Fix: api url config variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiURL: process.env.API_URL || "https://staging-core-api.lancecredit.com"
+    }
+  },
   components: [
     { path: '~/components/form', prefix: 'Form' },
     { path: '~/components/auth', prefix: 'Auth' },
