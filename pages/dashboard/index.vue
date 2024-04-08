883466464f9d874f09fd615dab9a4b8620fa74e0
Tolu ISOLA <i_tolu@yahoo.com>
2024-04-08 18:25:12 +0100

    :sparkles: Feat: Log out

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Logged In</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+    definePageMeta({
+        middleware: 'auth',
+        layout: 'dashboard'
+    });
+
+</script>
