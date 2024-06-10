1bc41530e6e1d23514f31e7e0f3a7a876b5d8864
Tolu ISOLA <i_tolu@yahoo.com>
2024-06-10 07:14:17 +0100

    :wrench: Chore: Page Title

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+
+<script setup lang="ts">
+
+  useHead({
+      title: 'Lance Credit | Redefining Credit for the African Market',
+    })
+
+</script>
