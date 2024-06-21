59b07031f6926730367f095f991f7cc9472ba871
Tolu ISOLA <i_tolu@yahoo.com>
2024-06-22 00:40:14 +0100

    :wrench: Chore: change auth ttl to 30 minutes

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -11,7 +11,7 @@ export default NuxtAuthHandler({
         signIn: '/auth/login'
     },
     session: {
-        maxAge: 3600
+        maxAge: 1800
     },
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
