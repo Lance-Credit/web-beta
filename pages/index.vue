46cef6e843343d06cc54849f93b723064325b23a
Tolu ISOLA <i_tolu@yahoo.com>
2024-07-28 14:43:02 +0100

    :bug: Fix: formatted phone number for backend payload

--- pages/index.vue
+++ pages/index.vue
@@ -288,6 +288,15 @@
         }
     });
 
+    const formattedPhoneNumberForPayload = computed(() => {
+        if ((signupFormValues.phoneNumber).substring(0,1) == '0') {
+            return '234'+(signupFormValues.phoneNumber).substring(1);
+        } else if ((signupFormValues.phoneNumber).substring(0,4) == '+234') {
+            return (signupFormValues.phoneNumber).substring(1);
+        }
+        return signupFormValues.phoneNumber;
+    })
+
     const signupErrorResponse: Ref<string> = ref('');
 
     const authToken: Ref<string> = ref('');
@@ -306,8 +315,8 @@
                 "email": signupFormValues.email,
                 "lastName": signupFormValues.lastName,
                 "password": signupFormValues.password,
-                "mobile": '234'+(signupFormValues.phoneNumber).substring(1),
                 "firstName": signupFormValues.firstName,
+                "mobile": formattedPhoneNumberForPayload.value,
                 "communityCode": signupFormValues.communityCode
             }
         );
