f6cd9c6bf67fc4eead0ef9a79cce2f107b6452dc
itolutheseedfi <i_tolu@yahoo.com>
2025-04-20 18:25:18 +0100

    :recycle: Refactor: disable small font input zoom on ios

--- app.vue
+++ app.vue
@@ -9,6 +9,9 @@
 
   useHead({
       title: 'Lance Credit | Redefining Credit for the African Market',
+      meta : [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+      ]
     })
 
 </script>
